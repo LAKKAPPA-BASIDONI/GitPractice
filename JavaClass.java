dec7de6024949ea4a36fd4bbea7d193507d74ce7
Lakkappa Basidoni <lakkappabasidoni2000@gmail.com>
2024-01-17 10:42:26 +0530

    second commit

--- JavaClass.java
+++ JavaClass.java
@@ -2,6 +2,6 @@ public class JavaClass{
 
 
     public static void main(String[] args){
-        System.out.println("Well come to java world..........!");
+        System.out.println("Well come to java world..........!!");
     }
 }
